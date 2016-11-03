6a6b1a10a94bdd2f16a994503802fe2a827e08b0
Joshdav <joshdav42@gmail.com>
2016-11-02 18:01:46 -0700

    Added regex
    Now matches the shrugface when next to other text, and typed as
    "¯\\_(ツ)_/¯" using regex.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import praw
 import OAuth2Util
 import time
+import re
 
 UA = "Shrug fixer by /u/AtomicEleven"
 r = praw.Reddit(UA)
@@ -27,30 +28,34 @@ The reason for this is that the underscore character (this one \\_ ) is used to 
 ***CAUTION: Probably very boring edit as to why you don't need to escape the second underscore, read only if you're super bored or need to fall asleep.***
 
 Edit: The reason you only need an escape character for the first underscore and not the second is because the second underscore (which doesn't have an escape character) doesn't have another underscore with which to italicize.  Reddit's formatting works in that you need a special character to indicate how you want to format text, then you put the text you want to format, then you put the character again.  For example, you would type \_italicize\_ or \*italicize\* in order to get _italicize_.  Since we put an escape character we have \\\\\_italicize\_ and don't need to escape the second underscore since there's not another non-escaped underscore with which to italicize something in between them.  So technically you could have written ¯\\\\\\\\\\\\\\_(ツ)\\\\\_/¯ but you don't need to since there's not a second non-escaped underscore.  You ***would*** need to escape the second underscore if you planned on using another underscore in the same line (but not if you used a line break, aka pressed enter twice).  If you used an asterisk later though on the same line it would not work with the non-escaped underscore to italicize.  To show you this, you can type _italicize* and it should not be italicized."""
+cache = [reply]
 
 #Main function which divides the comments and finds one to reply to
 def run_bot(): 
-	cache = [reply]
 	sub = r.get_subreddit('all')
 	comments = sub.get_comments(limit = 100)
 	for c in comments:
-		if shrug(c) and c.id not in cache and c not in cache:
+		if shrug(c) and c.id not in cache and c.body not in cache:
 			c.reply(reply)
 			cache.append(c.id)
 
 #Decides if the bot should reply based on each word in the comment
 def shrug(c):
 	text = c.body
-	tokens = text.split()
-	if "¯\_(ツ)_/¯" in tokens:
+	pattern1 = re.compile('(¯\\\\_\(ツ\)_\/¯)', re.UNICODE)
+	pattern2 = re.compile('(¯\\\\\\\\_\(ツ\)_\/¯)', re.UNICODE)
+	if re.search(pattern1, text) or re.search(pattern2, text):
 		return True
+	#tokens = text.split()
+	#if "¯\_(ツ)_/¯" in tokens:
+		#return True
 
 if __name__ == '__main__':
 	while True:
 		try:
 			run_bot()
-			time.sleep(30)
+			time.sleep(15)
 			o.refresh(force=True)
 		except (praw.errors.HTTPException, ConnectionError, praw.errors.Forbidden, ConnectionResetError, praw.errors.InvalidComment) as e:
-			time.sleep(30)
+			time.sleep(15)
 			o.refresh(force=True)
